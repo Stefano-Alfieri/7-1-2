d211eacb23f9b26d94458c68368a3648d76568e5
Stefano Alfieri <stefanoalfieri7@gmail.com>
2024-05-15 11:59:01 +0200

    progetto finito e bug corretti

--- src/org/generation/italy/setteEMezzo.java
+++ src/org/generation/italy/setteEMezzo.java
@@ -11,10 +11,13 @@ public class setteEMezzo {
 		Scanner sc = new Scanner(System.in);
 		Random r = new Random();
 		// dichiarazione dati e arrayList
-		String risposta = "";
-		float bilancio, punteggio = 0, puntata;
+		String risposta = "", rispostaRestart = "";
+		float bilancio, punteggio = 0, punteggioBanco = 0, puntata;
 		ArrayList<Integer> carte = new ArrayList<Integer>();
 		HashSet<Integer> carteEstratte = new HashSet<Integer>();
+		int randomCard;
+		String seme;
+		boolean giocVin = false;
 
 		// generazione mazzo carte
 		for (int i = 0; i < 40; i++) {
@@ -29,71 +32,136 @@ public class setteEMezzo {
 				System.out.println("Impossibile sedersi al tavolo con un bilancio negativo");
 		} while (bilancio < 0);
 		do {
-			// richiesta puntata
-			System.out.println("Inserisci la tua puntata:");
-			puntata = sc.nextFloat();
-			sc.nextLine();
-			if (puntata < 0)
-				System.out.println("Impossibile puntare una somma di denaro pari o inferiore a zero");
-			else if (puntata > bilancio)
-				System.out.println("Impossibile puntare una somma di denaro maggiore di quella che possiedi già");
-		} while (puntata < 0 || puntata > bilancio);
-		bilancio = bilancio - puntata;
-
-		// estrazione carta utente carta computer
-		do {
-			int randomCard;
-			String seme;
 			do {
-				// estrazione carta e attribuzione seme
-				randomCard = r.nextInt(40); // generazione num da 0 a 39
-			} while (carteEstratte.contains(randomCard));
-			carteEstratte.add(randomCard);
-			if (randomCard <= 9) {
-				randomCard = randomCard + 1;
-				seme = "bastoni";
-				if (randomCard == 0) {
-					randomCard = 10;
+				// richiesta puntata
+				System.out.println("Inserisci la tua puntata:");
+				puntata = sc.nextFloat();
+				sc.nextLine();
+				if (puntata < 0)
+					System.out.println("Impossibile puntare una somma di denaro pari o inferiore a zero");
+				else if (puntata > bilancio)
+					System.out.println("Impossibile puntare una somma di denaro maggiore di quella che possiedi già");
+			} while (puntata < 0 || puntata > bilancio);
+			// estrazione carta giocatore
+			do {
+				do {
+					// estrazione carta e attribuzione seme
+					randomCard = r.nextInt(40); // generazione num da 0 a 39
+				} while (carteEstratte.contains(randomCard));
+				carteEstratte.add(randomCard);
+				if (randomCard <= 9) {
+					randomCard = randomCard + 1;
+					seme = "bastoni";
+					if (randomCard == 0) {
+						randomCard = 10;
+					}
+				} else if (randomCard <= 19) {
+					randomCard = (randomCard + 1) % 10;
+					seme = "denari";
+					if (randomCard == 0) {
+						randomCard = 10;
+					}
+				} else if (randomCard <= 29) {
+					randomCard = (randomCard + 1) % 10;
+					seme = "spade";
+					if (randomCard == 0) {
+						randomCard = 10;
+					}
+				} else {
+					randomCard = (randomCard + 1) % 10;
+					seme = "coppe";
+					if (randomCard == 0) {
+						randomCard = 10;
+					}
 				}
-			} else if (randomCard <= 19) {
-				randomCard = (randomCard + 1) % 10;
-				seme = "denari";
-				if (randomCard == 0) {
-					randomCard = 10;
+				System.out.println("la tua carta è " + carte.get(randomCard) + " di " + seme);
+
+				// punteggio
+				if (randomCard <= 7) {
+					punteggio = punteggio + randomCard;
+				} else if (randomCard > 7) {
+					punteggio = punteggio + 0.5f;
 				}
-			} else if (randomCard <= 29) {
-				randomCard = (randomCard + 1) % 10;
-				seme = "spade";
-				if (randomCard == 0) {
-					randomCard = 10;
+				System.out.println("il tuo punteggio attuale è " + punteggio);
+				if (punteggio > 7.5f) {
+					System.out.println("Hai sballato!");
+					break;
+				} else if (punteggio < 7.5f) {
+					System.out.println("vuoi un'altra carta?");
+					risposta = sc.nextLine();
+				} else if (punteggio == 7.5f) {
+					System.out.println("hai raggiunto il punteggio massimo");
+					break;
 				}
+			} while (risposta.equalsIgnoreCase("si") || risposta.equalsIgnoreCase("sì"));
+
+			// banco 
+			do {
+				do {
+					// estrazione carta e attribuzione seme banco
+					randomCard = r.nextInt(40); // generazione num da 0 a 39
+				} while (carteEstratte.contains(randomCard));
+				carteEstratte.add(randomCard);
+				if (randomCard <= 9) {
+					randomCard = randomCard + 1;
+					seme = "bastoni";
+					if (randomCard == 0) {
+						randomCard = 10;
+					}
+				} else if (randomCard <= 19) {
+					randomCard = (randomCard + 1) % 10;
+					seme = "denari";
+					if (randomCard == 0) {
+						randomCard = 10;
+					}
+				} else if (randomCard <= 29) {
+					randomCard = (randomCard + 1) % 10;
+					seme = "spade";
+					if (randomCard == 0) {
+						randomCard = 10;
+					}
+				} else {
+					randomCard = (randomCard + 1) % 10;
+					seme = "coppe";
+					if (randomCard == 0) {
+						randomCard = 10;
+					}
+				}
+				System.out.println("la carta del banco è " + carte.get(randomCard) + " di " + seme);
+
+				// punteggio banco
+				if (randomCard <= 7) {
+					punteggioBanco = punteggioBanco + randomCard;
+				} else if (randomCard > 7) {
+					punteggioBanco = punteggioBanco + 0.5f;
+				}
+				System.out.println("il punteggio attuale del banco è " + punteggioBanco);
+				if (punteggioBanco > 7.5f) {
+					System.out.println("il banco ha sballato!");
+					System.out.println("il giocatore ha vinto");
+					giocVin = true;
+					break;
+				} else if (punteggio > 7.5) {
+					System.out.println("Il banco ha vinto");
+					break;
+				} else if (((punteggioBanco < 7.5f && punteggio <= 7.5f))
+						|| (punteggioBanco < punteggio && punteggio <= 7.5f)) {
+					System.out.println("il banco chiede un'altra carta");
+				}
+			} while ((punteggioBanco < punteggio) && (punteggioBanco <= 7.5f));
+			if (giocVin == true) {
+				bilancio = bilancio + puntata;
 			} else {
-				randomCard = (randomCard + 1) % 10;
-				seme = "coppe";
-				if (randomCard == 0) {
-					randomCard = 10;
-				}
+				bilancio = bilancio - puntata;
 			}
-			System.out.println("la tua carta è " + carte.get(randomCard) + " di " + seme);
-
-			// punteggio
-			if (randomCard <= 7) {
-				punteggio = punteggio + randomCard;
-			} else if (randomCard > 7) {
-				punteggio = punteggio + 0.5f;
-			}
-			System.out.println("il tuo punteggio attuale è " + punteggio);
-			if (punteggio > 7.5f) {
-				System.out.println("Hai sballato!");
-				break;
-			} else if (punteggio < 7.5f) {
-				System.out.println("vuoi un'altra carta?");
-				risposta = sc.nextLine();
-			} else if (punteggio == 7.5f) {
-				System.out.println("hai raggiunto il punteggio massimo");
-			}
-		} while (risposta.equalsIgnoreCase("si") || risposta.equalsIgnoreCase("sì"));
-
+			punteggio = 0;
+			punteggioBanco = 0;
+			System.out.println("il tuo bilancio attuale " + bilancio);
+			//restart
+			System.out.println("vuoi continuare a giocare?");
+			rispostaRestart = sc.nextLine();
+		} while (rispostaRestart.equalsIgnoreCase("si") || rispostaRestart.equalsIgnoreCase("sì"));
+		System.out.println("Grazie, arrivederci");
 	}// fine main
 
 }
